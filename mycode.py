5e4d3b0aa541c437f80174c6072c9503548464a4
wpacocha <77246645+wpacocha@users.noreply.github.com>
2025-03-12 16:21:40 +0100

    Delete mycode.py

--- mycode.py
+++
@@ -1,8 +0,0 @@
-import re
-def Add(numbers):
-    if not numbers:
-        return 0
-    if numbers.endswith('\n') or numbers.endswith(','):
-        raise ValueError
-    numbers_list = re.split(r'[, \n]',numbers)
-    return sum(map(int, numbers_list))
